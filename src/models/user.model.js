3974d0a0e3ff4a855f11bb8ee5cc2064a5d30250
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-08-25 17:28:08 +0530

    feat(user) : add compare method in model itself using bcrypt.compare

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,4 +57,8 @@ userSchema.pre("save" , async function (next){
     }
 });
 
+userSchema.methods.isPassCorrect= async function(password){
+    return await bcrypt.compare(password , this.password);
+}
+
 export const User = mongoose.model("User" , userSchema);
