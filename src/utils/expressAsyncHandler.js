b204e84b0179f916c1682b9371acddb8c4e65ca6
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-08-26 17:02:12 +0530

    feat(try-catch wrapper) : add a exprexx async handler to handel api call with error management

---
+++ src/utils/expressAsyncHandler.js
@@ -0,0 +1,19 @@
+/**
+ * A higher order function wrapper which return a executable function wrapperd in trycatch
+ * 
+ * @param {function} fn -a async function to be wrwpped in try-catch block
+ * @returns {Promise<void>}
+ * @throws {error} -throw error if function exe fails to express's error handling middleware
+ */
+
+const expressAsyncHandler =(fn)=>{
+    return async (req , res , next)=>{
+        try {
+            await fn(req , res , next);
+        } catch (error) {
+            console.log("operation failed in EXPRESSasyncHandler , ERROR : " , error);
+            throw error;
+        }
+    }
+    
+}
