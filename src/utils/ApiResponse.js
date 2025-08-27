831fe52710a3fda56e4861a7f303a7efabe35625
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-08-27 16:02:10 +0530

    feat(errors): add ApiError class for structured error handling feat(response): add ApiResponse class for standardized API responses

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+//copy paste-will learn later
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
+export { ApiResponse }
