448d46408991c6e4787cf39affbc81342ad7d52a
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-09-23 19:29:05 +0530

    feat(video): controlller function to publish video , handle multer and cloudinary for thumbnail and videoFile

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,58 @@
+import { Video } from "../models/video.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { cloudinaryUpload } from "../utils/cloudinary.js";
+import { expressAsyncHandler } from "../utils/expressAsyncHandler.js";
+
+const publishVideo = expressAsyncHandler(async(req , res)=>{
+   
+    const videoFileLocalPath = req.files?.videoFile[0].path;
+    const thumbnailLocalPath = req.files?.thumbnail[0].path;
+    //console.log("req files object : " , req.files);
+    
+
+    if(!videoFileLocalPath || !thumbnailLocalPath) throw new ApiError(404 , "either videoFile or thumbnail not found");
+
+    const{title , description} = req.body;
+
+    const videoObject=await cloudinaryUpload(videoFileLocalPath);
+    const thumbnailObject =await cloudinaryUpload(thumbnailLocalPath);
+
+    //console.log("cloudinary response object videoFile : " , videoObject);
+    //console.log("cloudinary response object thumbnail : " , thumbnailObject);
+
+    if(!videoObject) throw new ApiError(401 , "unable to upload video on cloudinary");
+    if(!thumbnailObject) throw new ApiError(401 , "unable to upload thumbnail on cloudinary");
+
+    const video = await Video.create({
+            title : title ,
+            videoFile : videoObject.playback_url ,
+            thumbnail : thumbnailObject.secure_url ,
+            description : description ,
+            duration : videoObject.duration ,
+            views : 0 ,
+            isPublished : false ,
+            owner : req.user ,
+    });
+
+    const createdVideo = await Video.findById(video._id);
+
+    if(!createdVideo) throw new ApiError(500 , "unable to create video");
+
+
+    return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200 ,
+                    createdVideo ,
+                    "Video ceated successfully"
+                )
+            )
+})
+
+
+export {
+    publishVideo , 
+
+}
