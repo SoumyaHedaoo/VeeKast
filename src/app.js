1bd7438efb8e9cdb02706d20312d50fb4cc1d957
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-09-28 17:37:53 +0530

    feat(comment): add basic setup for comment routes and controller for handling comments

--- src/app.js
+++ src/app.js
@@ -25,9 +25,11 @@ app.use(cookieParser()); // makes users cookie accessible
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.route.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 app.use("/api/v1/user" , userRouter);
 app.use("/api/v1/video" , videoRouter);
 app.use("/api/v1/tweet" , tweetRouter);
+app.use("/api/v1/comment" , commentRouter);
 
 export {app};
